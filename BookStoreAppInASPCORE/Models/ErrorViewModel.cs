4b05ff4dd05973597df5b3f2b71f8a3f93ec81b5
Jalil T <jalil.tahirov@outlook.com>
2018-08-13 21:32:43 +0400

    created aspnetcortwo project and added some css for migration

---
+++ BookStoreAppInASPCORE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookStoreAppInASPCORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
